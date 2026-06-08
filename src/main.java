4f175734fabc179e3b7d782e792868611e764895
DarkSnake749 <etienne.boule@hotmail.com>
2026-06-07 22:34:38 -0400

    Testing the imports

--- src/main.java
+++ src/main.java
@@ -1,9 +1,12 @@
 package SpaceInvader.src;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class main {
     public static void main(String args[]) {
         System.out.println("Hello, world!");
 
     }
-    
+
 }
